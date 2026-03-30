ece3548ddb69108424f60a58580f17bfc2bba841
Anarkythera <d1videsb1zer0@gmail.com>
2026-03-30 01:36:12 +0100

    Parse correlation ID from the response

--- app/main.go
+++ app/main.go
@@ -4,16 +4,19 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
+
+var SERVER_PORT = 9092
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// TODO: Uncomment the code below to pass the first stage
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", SERVER_PORT))
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
@@ -24,37 +27,61 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	handleConnection(conn)
+	resp, err := handleConnection(conn)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		os.Exit(-1)
+	}
+
+	conn.Write(resp)
 
 }
 
-func handleConnection(conn net.Conn) {
+/*
+ * all resp or req msg have a size field of 32bit
+ */
+func handleConnection(conn net.Conn) ([]byte, error) {
 
-	buf := make([]byte, 4096)
+	buf := make([]byte, 0, 4096)
+	tmp := make([]byte, 4096)
 
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+	for {
+		n, err := conn.Read(tmp)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		buf = append(buf, tmp[:n]...)
 	}
 
+	fmt.Printf("%d\n", buf)
+
 	// response, message_size (4 bytes), correlation_id (4 bytes)
-	// correlation_id hardcoded to 7 for now
+	// msg_size hardcoded to 0 now
 
 	resp := []byte{0, 0, 0, 0}
 
 	var buff bytes.Buffer
-	correlationId := int32(7)
+	correlationId := extractCorrelationId(buf)
+	fmt.Printf("Correlation ID of the msg: %d \n", correlationId)
 
-	err = binary.Write(&buff, binary.BigEndian, correlationId)
-
+	err := binary.Write(&buff, binary.BigEndian, correlationId)
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 	}
 
 	resp = append(resp, buff.Bytes()...)
 
-	conn.Write(resp)
-	conn.Close()
+	return resp, nil
+}
 
+func extractCorrelationId(buf []byte) int32 {
+
+	correlationId := int32(binary.BigEndian.Uint32(buf[8:12]))
+
+	return correlationId
 }
